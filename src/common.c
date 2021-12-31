2bbf20a03b23ff1c640d654f1d13b59d00d74b1b
Sebastian Krzyszkowiak <dos@dosowisko.net>
2021-12-31 22:46:00 +0100

    initial commit

---
+++ src/common.c
@@ -0,0 +1,43 @@
+/*! \file common.c
+ *  \brief Common stuff that can be used by all gamestates.
+ */
+/*
+ * Copyright (c) Sebastian Krzyszkowiak <dos@dosowisko.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "common.h"
+#include <libsuperderpy.h>
+
+bool GlobalEventHandler(struct Game* game, ALLEGRO_EVENT* ev) {
+	if ((ev->type == ALLEGRO_EVENT_KEY_DOWN) && (ev->keyboard.keycode == ALLEGRO_KEY_M)) {
+		ToggleMute(game);
+	}
+
+	if ((ev->type == ALLEGRO_EVENT_KEY_DOWN) && (ev->keyboard.keycode == ALLEGRO_KEY_F)) {
+		ToggleFullscreen(game);
+	}
+
+	return false;
+}
+
+struct CommonResources* CreateGameData(struct Game* game) {
+	struct CommonResources* data = calloc(1, sizeof(struct CommonResources));
+	return data;
+}
+
+void DestroyGameData(struct Game* game) {
+	free(game->data);
+}
